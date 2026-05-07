25c53aa50c12b0e4bef1376a83e192a1a400f1a0
Kiikiki <allysiakiara@gmail.com>
2026-05-07 08:51:27 +0700

    UMm CyberSecurity threat I committed my key by accident please no more...

--- task automation agent (unique)/agent.py
+++ task automation agent (unique)/agent.py
@@ -1,37 +1,50 @@
-from openai import OpenAI
+from google import genai
 from emailTool import authenticateGmail, getEmails, thisWeekEmail
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+apiKey = os.getenv("GEMINI_API_KEY")
 
 # connection to an LLM
-client = OpenAI()
+client = genai.Client(api_key="apiKey")
 
-response = client.chat.completions.create(
-  model="gpt-4.1",
-    messages=[
-        { "role": "user", "content": "Remind me of of my schedule, tasks and organize my emails" }
-    ]
+# test LLM connection (works!)
+# response = client.models.generate_content(
+#     model = "gemini-2.5-flash",
+#     contents = "say hello to the world!" 
+# )
+
+# print(response.text)
+
+response = client.models.generate_content(
+    model = "gemini-2.5-flash",
+    contents = ""
 )
 
-print(response.choices[0].message.content)
+# what the LLM does:
 
-scopes = ['https://www.googleapis.com/auth/gmail.readonly']
+# email tool
+# scopes = ['https://www.googleapis.com/auth/gmail.readonly']
 
-servicePersonal = authenticateGmail('credentialsPersonal.json', 8080)
-serviceWork = authenticateGmail('credentialsWork.json', 0)
+# servicePersonal = authenticateGmail('credentialsPersonal.json', 8080)
+# serviceWork = authenticateGmail('credentialsWork.json', 0)
 
-print("\nAuthentication successful for both accounts!")
+# print("\nAuthentication successful for both accounts!")
 
-personalMails = getEmails(servicePersonal)
-print("\nFetched personal emails...")
+# personalMails = getEmails(servicePersonal)
+# print("\nFetched personal emails...")
 
-workMails = getEmails(serviceWork)
-print("\nFetched work emails.")
+# workMails = getEmails(serviceWork)
+# print("\nFetched work emails.")
 
-emails = personalMails + workMails
-print("\nCompiled all emails from both accounts.")
+# emails = personalMails + workMails
+# print("\nCompiled all emails from both accounts.")
 
-recentEmails = thisWeekEmail(emails)
-print("\nFiltered emails from the last week.")
-print(f"\nYou have {len(recentEmails)} emails from the last week.")
+# recentEmails = thisWeekEmail(emails)
+# print("\nFiltered emails from the last week.")
+# print(f"\nYou have {len(recentEmails)} emails from the last week.")
 
 
 
